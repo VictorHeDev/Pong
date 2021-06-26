6d66b206217621c6d17eead08bc399a38b2c944c
Woodstock <victorhe97@gmail.com>
2021-06-25 22:09:18 -0400

    Rendered the different classes such as Paddle, Net, and Ball

--- classObjects.js
+++ classObjects.js
@@ -16,36 +16,154 @@ WHen you hve a class, you can use the class to create new objects
 Do I need to make a class Canvas?
 */
 
+// event listeners are global --> move to Pong 
+
+
+// make aiPaddle work
+// ball render and appear
+// draw net
+// draw scores  keep inside game class (Pong) as global var
+// collision (last part)
+
 class Paddle {
-    constructor(x, y = canvas.height / 2 - 50, width = 10, height = 100, color = 'white') {
+    constructor(x, y, ctx, speed, canvasHeight, width=10, height=100, color='white') {
         this.x = x;
         this.y = y;
+        this.ctx = ctx;
+        this.speed = speed;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.canvasHeight = canvasHeight;
+        this.direction = 0;
     }
 
-    drawPaddle(x, y, width, height, color) {
-        this.fillStyle = color;
-        this.ctx.fillRect(x, y, width, height)
+    render() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 
-    window.addEventListener('keydown', keyDownHandler);
-    window.addEventListener('keyup', keyUpHandler);
-    
     move(upArrowPressed, downArrowPressed) {
+        if (upArrowPressed && this.y > 0) {
+            this.y -= 8;
+        } else if (downArrowPressed && (this.y < this.canvasHeight - this.height)) {
+            this.y += 8;
+        }
+    }
+}
+
+class Net {
+    constructor(width, height, ctx, x, y, color) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.ctx = ctx;
+        
+    }
+
+    render() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Ball {
+    constructor(x, y , ctx, radius, speed, velocityX, velocityY, color, canvasHeight, canvasWidth) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = speed;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.color = color;
+        this.ctx = ctx;
+        this.canvasHeight;
+        this.canvasWidth;
+    }
+
+    render() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
+
+    reset() { //in update function
+        // reset ball's value to older values (position)
+        this.x = this.canvasWidth / 2;
+        this.y = this.canvasHeight / 2;
+        this.speed = 7;
+    
+        // changes the direction of the ball
+        this.velocityX = -this.velocityX;
+        this.velocityY = -this.velocityY;
+    }
+}
+
+// class Score {
+//     constructor(x, y, score = 0, color = 'white', font = '35px san-serif') {
+//         this.x = x;
+//         this.y = y;
+//         this.score = score;
+//         this.color = color;
+//         this.font = font;
+//     }
+
+//     drawScore(x, y, score = 0) {
+//         this.ctx.fillStyle = this.color;
+//         this.ctx.font = font;
+//         this.ctx.fillText(score, x, y);
+//     }
+
+//     
+// }
+
+// let upArrowPressed = false;
+// let downArrowPressed = false;
+
+
+
+class Pong { // run the game 
+    constructor(canvasId, fps) {
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext('2d'); 
+        this.fps = fps;
+        this.paddleSpeed = 8;
+        this.userPaddle = new Paddle(10, this.canvas.height / 2 - 50, this.ctx, this.paddleSpeed, this.canvas.height);
+        this.aiPaddle = new Paddle(this.canvas.width - 20, this.canvas.height / 2 - 50, this.ctx, this.paddleSpeed, this.canvas.height);
+        this.net = new Net(4, this.canvas.height, this.ctx, this.canvas.width / 2 - 2, 0, 'white');
+        this.ball = new Ball(canvas.width / 2, canvas.height / 2, this.ctx, 7, 7, 5, 5, 'cyan', this.canvas.height, this.canvas.width);
+        // this.userScore = new Score();
+        // this.aiScore = new Score();
+        this.upArrowPressed = false;
+        this.downArrowPressed = false;
+
+        this.keyDownHandler = this.keyDownHandler.bind(this);
+        this.keyUpHandler = this.keyUpHandler.bind(this);
+
+        window.addEventListener('keydown', this.keyDownHandler);
+        window.addEventListener('keyup', this.keyUpHandler);   
+        
+        // if under, bind to window
 
     }
 
+    // movement
     keyDownHandler(event) {
         switch (event.keyCode) {
             // 'up arrow' key
             case 38: 
-                upArrowPressed = true;
+                this.userPaddle.direction = -1;
+                this.upArrowPressed = true;
+                
                 break;
             // 'down arrow' key
             case 40:
-                downArrowPressed = true;
+                this.userPaddle.direction = 1;
+                this.downArrowPressed = true;
                 break;
         }
     }
@@ -54,173 +172,100 @@ class Paddle {
         switch (event.keyCode) {
             // 'up arrow' key
             case 38: 
-                upArrowPressed = false;
+                this.userPaddle.direction = 0;
+                this.upArrowPressed = false;
                 break;
             // 'down arrow' key
             case 40:
-                downArrowPressed = false;
+                this.userPaddle.direction = 0;
+                this.downArrowPressed = false;
                 break;
         }
     }
-
-}
-
-class Net {
-    constructor(width = 4, height = canvas.height, x = canvas.width / 2 - width / 2, y = 0, color = 'white') {
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
-
-    drawNet() {
-        this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
-
-class Ball {
-    constructor(x = canvas.width / 2, y = canvas.height / 2, radius = 7, speed = 7, velocityX = 5, velocityY = 5, color = 'cyan') {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.speed = speed;
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
-        this.color = color;
-    }
-    drawBall(x, y, radius, color) {
-        this.fillStyle = color;
-        this.ctx.beginPath();
-
-        this.ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-        this.ctx.closePath();
-        this.ctx.fill();
-    }
-}
-
-class Score {
-    constructor(x, y, score = 0, color = 'white', font = '35px san-serif') {
-        this.x = x;
-        this.y = y;
-        this.score = score;
-        this.color = color;
-        this.font = font;
-    }
-
-    drawScore(x, y, score = 0) {
-        this.ctx.fillStyle = this.color;
-        this.ctx.font = font;
-        this.ctx.fillText(score, x, y);
-    }
-
-    reset() {
-        // reset ball's value to older values (position)
-        this.ball.x = canvas.width / 2;
-        this.ball.y = canvas.height / 2;
-        this.ball.speed = 7;
-    
-        // changes the direction of the ball
-        this.ball.velocityX = -ball.velocityX;
-        this.ball.velocityY = -ball.velocityY;
-    }
-}
-
-
-class Pong { // run the game 
-    constructor(canvasId, fps) {
-        this.canvas = document.getElementById(canvasId);
-        this.ctx = canvas.getContext('2d'); 
-        this.fps = fps;
-        this.userPaddle = new Paddle();
-        this.aiPaddle = new Paddle();
-        this.net = new Net();
-        this.ball = new Ball();
-        this.userScore = new Score();
-        this.aiScore = new Score();
-
-    }
-
     // game functions
-    drawCanvas() {
+    renderCanvas() {
         this.ctx.fillStyle = "black";
-        this.ctx(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
     }
+
     update() {
-        if (upArrowPressed && userPaddle.y > 0) {
-            userPaddle.y -= 8;
-        } else if (downArrowPressed && (userPaddle.y < canvas.height - userPaddle.height)) {
-            userPaddle.y += 8;
-        }
-        if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0) {
-            ball.velocityY = -ball.velocityY
+        this.userPaddle.move(this.upArrowPressed, this.downArrowPressed);
+
+        if (this.ball.y + this.ball.radius >= this.canvas.height || this.ball.y - this.ball.radius <= 0) {
+            this.ball.velocityY = -this.ball.velocityY
         }
     
-        if (ball.x + ball.radius >= canvas.width) {
-            userPaddle.score += 1;
-            reset();
+        if (this.ball.x + this.ball.radius >= this.canvas.width) {
+            // userPaddle.score += 1;
+            this.ball.reset();
         }
     
-        if (ball.x - ball.radius <= 0) {
-            aiPaddle.score += 1;
-            reset();
+        if (this.ball.x - this.ball.radius <= 0) {
+            // aiPaddle.score += 1;
+            this.ball.reset();
         }
-        ball.x += ball.velocityX;
-        ball.y += ball.velocityY;
+        this.ball.x += this.ball.velocityX;
+        this.ball.y += this.ball.velocityY;
 
-        aiPaddle.y += ((ball.y - (aiPaddle.y + aiPaddle.height / 2))) * 0.09; 
+        // aiPaddle.y += ((ball.y - (aiPaddle.y + aiPaddle.height / 2))) * 0.09; 
 
-        let player = (ball.x < canvas.width / 2) ? userPaddle : aiPaddle;
+        // let player = (ball.x < canvas.width / 2) ? userPaddle : aiPaddle;
 
-        if (collisionDetect(player, ball)) {
-            let angle = 0; // default angle is 0deg radian
+        // if (collisionDetect(player, ball)) {
+        //     let angle = 0; // default angle is 0deg radian
 
-            if (ball.y < (player.y + player.height / 2)) {
-                angle = -1 * Math.PI / 4; // -45 deg
-            } else if (ball.y > (player.y + player.height / 2)) {
-                angle = Math.PI / 4; // 45 deg
-            }
+        //     if (ball.y < (player.y + player.height / 2)) {
+        //         angle = -1 * Math.PI / 4; // -45 deg
+        //     } else if (ball.y > (player.y + player.height / 2)) {
+        //         angle = Math.PI / 4; // 45 deg
+        //     }
             
-            ball.velocityX = (player === userPaddle ? 1 : -1) * ball.speed * Math.cos(angle);
-            ball.velocityY = ball.speed * Math.sin(angle);
+        //     ball.velocityX = (player === userPaddle ? 1 : -1) * ball.speed * Math.cos(angle);
+        //     ball.velocityY = ball.speed * Math.sin(angle);
         
-            ball.speed += 0.2;
-        }
+        //     ball.speed += 0.2;
+        // }
     
     }
+
+    // render canvas and paddle only
+    // don't worry about collison and score for now
 
     render() {
         // set a style
-        ctx.fillStyle = "#black"; 
-    
-        this.drawNet();
-        this.drawScore(canvas.width / 4, canvas.height / 6, userPaddle.score);
-        this.drawScore(3 * canvas.width / 4, canvas.height / 6, aiPaddle.score);
-        this.drawPaddle(userPaddle.x, userPaddle.y, userPaddle.width, userPaddle.height, userPaddle.color);
-        this.drawPaddle(aiPaddle.x, aiPaddle.y, aiPaddle.width, aiPaddle.height, aiPaddle.color);
-        this.drawBall(ball.x, ball.y, ball.radius, ball.color);
+        this.renderCanvas();
+        this.net.render();
+        // this.drawScore(canvas.width / 4, canvas.height / 6, userPaddle.score);
+        // this.drawScore(3 * canvas.width / 4, canvas.height / 6, aiPaddle.score);
+
+        this.userPaddle.render();
+        this.aiPaddle.render();
+        this.ball.render();
+        
     }
 
-    collisionDetect(player, ball) {
-        player.top = player.y;
-        player.right = player.x + player.width;
-        player.bottom = player.y + player.height;
-        player.left = player.x;
+    // collisionDetect(player, ball) {
+    //     player.top = player.y;
+    //     player.right = player.x + player.width;
+    //     player.bottom = player.y + player.height;
+    //     player.left = player.x;
     
-        ball.top = ball.y - ball.radius;
-        ball.right = ball.x + ball.radius;
-        ball.bottom = ball.y + ball.radius;
-        ball.left = ball.x - ball.radius;
+    //     ball.top = ball.y - ball.radius;
+    //     ball.right = ball.x + ball.radius;
+    //     ball.bottom = ball.y + ball.radius;
+    //     ball.left = ball.x - ball.radius;
     
-        return ball.left < player.right && ball.top < player.bottom && ball.right > player.left && ball.bottom > player.top;
-    }
+    //     return ball.left < player.right && ball.top < player.bottom && ball.right > player.left && ball.bottom > player.top;
+    // }
+
     gameLoop() {
         this.update();
         this.render();
     }
     
-    setInterval(gameLoop, 1000 / 60); // fps
+    start() {
+        setInterval(this.gameLoop.bind(this), this.fps); // fps -- look up what .bind does 
+    }
 
 }
 
